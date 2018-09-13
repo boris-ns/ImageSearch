2ed7245dade7d5da8771b0d46067b8c4c2c4eb2f
boris-ns <b.sulicenko@gmail.com>
2018-09-13 15:07:18 +0200

    Now it's possible to add and remove images from favourites. Those images will display right below searched images.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,6 +11,7 @@ export class AppComponent {
   title = 'Image Search';
   searchKeyword = '';
   resultImages = [];
+  favouriteImages = [];
   isLoading = false;
 
   constructor(private dataService: DataService) {
@@ -26,5 +27,24 @@ export class AppComponent {
         response.forEach(element => this.resultImages.push(element.urls.small));
       }
     );
+  }
+
+  onAddToFavourites(imageUrl: String) {
+    if (this.favouriteImages.indexOf(imageUrl) === -1) {
+      this.favouriteImages.push(imageUrl);
+    } else {
+      alert('This image is already in your favourites.');
+    }
+  }
+
+  onRemoveFromFavourites(imageUrl: String) {
+    const index = this.favouriteImages.indexOf(imageUrl);
+
+    if (index === -1) {
+      alert('Error while removing image from favourites.');
+      return;
+    }
+
+    this.favouriteImages.splice(index, 1);
   }
 }
